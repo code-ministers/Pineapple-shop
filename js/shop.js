9fbe051e440d4fe27b17ba9b675e49be373e32c0
ahmedbani <a13banisal@gmail.com>
2021-08-01 11:37:02 +0300

    update on shop.js

--- js/shop.js
+++ js/shop.js
@@ -6,3 +6,26 @@ const Toy = function(name,src) {
 Toy.all = [];
 
 
+const Cart = function(items) {
+  this.items = items;
+};
+
+const CartItem = function(toy, quantity) {
+  this.toy = toy;
+  this.quantity = quantity;
+};
+
+Cart.prototype.addItem = function (toy, quantity) {
+  let newItem  = new CartItem(toy,quantity);
+  this.items.push(newItem);
+
+};
+
+Cart.prototype.saveToLocalStorage = function () {
+  let stringArr = JSON.stringify(this.items);
+  localStorage.setItem('cart',stringArr);
+};
+
+Cart.prototype.removeItem = function(item) {
+  this.items.splice(item,1);
+};
