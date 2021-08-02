9439b8f8ff9d16cd6bb6e9842664fa2872a9b0ba
amani-alnobani <amanialnobani@gmail.com>
2021-08-02 09:58:58 +0300

    complete render function

--- js/product.js
+++ js/product.js
@@ -1,29 +1,41 @@
 
 'use strict';
 //slied show
+
+
+
+
+
+
+// Toy.all;
 //render fun
+//   function  renderFunction (){
 
-
-
-
-
-  function  renderFunction (){
-
-    let imageEl=document.getElementById('images');
+//     let imageEl=document.getElementById('images');
    
-    for (i=0; i<Toy.all.length; i++){
+//     for (let i=0; i<Toy.all.length; i++){
     
-        let data=document.createElement('img');
-        imageEl.appendChild(data);
-        data.src=Toy.all.src[i];
-          
-    }
-}
+//         let data=document.createElement('img');
+//         imageEl.appendChild(data);
+//         data.src=Toy.all.src[i];
+//         data.textContent=Toy.all.name[i];
+//         data.textContent=Toy.all.price[i];
+        
+//     }
+// }
 
-renderFunction();
-   // let lableEl=document.getElementById('filter');
+// renderFunction();
+
+//    let lableEl=document.getElementById('filter');
     
-    
+//    lableEl.addEventListener('selsct',forSelection);
+
+//    function forSelection(event) {
+//     event.preventDefault();
+//     let Boy = event.target.gender.value;
+
+//    }
+
     //let opatioEl1 = lableEl.options[lableEl.selectedIndex].value;
 
    // let opationEl2 = lableEl.options[lableEl.selectedIndex].;//
--- js/shop.js
+++ js/shop.js
@@ -1,25 +1,25 @@
 
-const Toy = function(name,src,price,gender) {
+const Toy = function (name, src, price, gender) {
   this.name = name;
   this.src = src;
-  this.price=price;
+  this.price = price;
   this.boyToy(gender);
   this.girlToy(gender);
 
   Toy.all.push(this);
 };
 Toy.all = [];
-Toy.boysToys=[];
-Toy.girlsToys=[];
+Toy.boysToys = [];
+Toy.girlsToys = [];
 
-Toy.prototype.boyToy = function(gender){
-  if(gender==='b'){
+Toy.prototype.boyToy = function (gender) {
+  if (gender === 'b') {
     Toy.boysToys.push(this);
   }
 };
 
-Toy.prototype.girlToy = function(gender){
-  if(gender==='g'){
+Toy.prototype.girlToy = function (gender) {
+  if (gender === 'g') {
     Toy.girlsToys.push(this);
   }
 };
@@ -61,10 +61,11 @@ function generateProduct() {
   new Toy('pizzle-truck', '/img/boy/puzzle-truck.png', '10JD', 'b');
   new Toy('ring-stack', '/img/boy/ring-stack.png', '3JD', 'b');
   new Toy('robot', '/img/boy/robot.png ', '12JD', 'b');
-  new Toy('rocking-horse', '/img/boy/school-bus.png ', '6JD', 'b');
+  new Toy('school-bus', '/img/boy/school-bus.png ', '6JD', 'b');
+  new Toy('rocking-horse', '/img/boy/rocking-horse.png', '20JD', 'b');
   new Toy('superMan', '/img/boy/superMan.png ', '7JD', 'b');
   new Toy('homeGirls', '/img/girl/homeGirls.png', '12JD', 'g');
-  new Toy('huorsForGirls', '/img/girl/huorsForGirls.png', '15JD', 'g');
+  new Toy('horseForGirls', '/img/girl/hoursForGirls.png', '15JD', 'g');
   new Toy('lego-minnie-mouse', '/img/girl/lego-minnie-mouse.png ', '15JD', 'g');
   new Toy('newGirls-toys', '/img/girl/newGirls-toys.png ', '7JD', 'g');
   new Toy('planForGirls', '/img/girl/planForGirls.png ', '5JD', 'g');
@@ -81,3 +82,44 @@ function generateProduct() {
 generateProduct();
 
 
+function renderFunction() {
+
+  let imageEl = document.getElementById('images');
+
+  for (let i = 0; i < Toy.all.length; i++) {
+    // console.log(Toy.all[i]);
+    let data = document.createElement('img');
+    let dataDescr = document.createElement('p')
+    let button=document.createElement('button')
+    imageEl.appendChild(data);
+    imageEl.appendChild(dataDescr);
+    imageEl.appendChild(button);
+    button.textContent='add to cart';
+    data.src = Toy.all[i].src;
+    dataDescr.textContent = `${Toy.all[i].name}     ${Toy.all[i].price}`;
+    // console.log(Toy.all[i].name + "   " +  Toy.all[i].price);
+    // console.log(dataDescr);
+  }
+}
+
+renderFunction();
+
+let lableEl = document.getElementById('filter');
+
+lableEl.addEventListener('select', forSelection);
+
+function forSelection(event) {
+  event.preventDefault();
+  Toy.all = 0;
+  let boy=event.target.gender.value
+  for (let i = 0; i < Toy.boysToys.length; i++) {
+
+    let dataBoy = document.createElement('img');
+    let dataDescr = document.createElement('p')
+    imageEl.appendChild(dataBoy);
+    imageEl.appendChild(dataDescr);
+    dataBoy.src =Toy.boysToys[i].src;
+    dataDescr.textContent = `${Toy.boysToys[i].name}     ${Toy.boysToys[i].price}`;
+  }
+
+}
